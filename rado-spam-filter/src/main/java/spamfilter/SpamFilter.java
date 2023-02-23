246bc55753368661b5a4edade17faabe8b6693ee
rado <citron0137@gmail.com>
2023-02-23 20:43:22 +0900

    Add SpamFilter And comment

---
+++ rado-spam-filter/src/main/java/spamfilter/SpamFilter.java
@@ -0,0 +1,15 @@
+package spamfilter;
+
+
+public class SpamFilter {
+    public boolean isSpam(String context, String[] spamLinkDomains, int redirectionDepth){
+        // 1. find url link from the context
+        // 2. for 10 times parse urls or redirect urls (BFS)
+        // 3. if any of the urls contains string in spamLinkDomains return true
+        // else return false
+
+        return false;
+    }
+
+
+}
